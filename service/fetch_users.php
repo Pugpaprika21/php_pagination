3c18b950da10d3d07ad6273265fe11bd323f7826
Pugpaprika21 <pugpaprika21@gmail.com>
2023-08-02 06:12:20 -0700

    e

---
+++ service/fetch_users.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once __DIR__ . "../../include/include.php";
+
+header("Access-Control-Allow-Headers: *");
+header("Access-Control-Allow-Origin: *");
+header("Content-type:application/json");
+
+$users = db_select("SELECT * FROM user_tb");
+$total = count($users);
+
+if (!empty($_GET['fetch_users'])) {
+    if ($total > 0) {
+        echo json_encode(['data' => $users, 'rows' => $total, 'status' => true]);
+        exit;
+    }
+    echo json_encode(['data' => $users, 'rows' => $total, 'status' => false]);
+    exit;
+}
