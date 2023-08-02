504c645a1f7b3a71d57a13c693ad7fa165c1c89d
Pugpaprika21 <pugpaprika21@gmail.com>
2023-08-02 06:36:54 -0700

    e

--- service/fetch_users.php
+++ service/fetch_users.php
@@ -4,7 +4,7 @@ require_once __DIR__ . "../../include/include.php";
 
 header("Access-Control-Allow-Headers: *");
 header("Access-Control-Allow-Origin: *");
-header("Content-type:application/json");
+header("Content-type: application/json");
 
 $users = db_select("SELECT * FROM user_tb");
 $total = count($users);
